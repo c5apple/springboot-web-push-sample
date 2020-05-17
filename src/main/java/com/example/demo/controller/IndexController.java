da2a1f76f446f34c80ed4f64ab5d76c2d7902a5b
c5apple <c5apple_party@yahoo.co.jp>
2020-05-17 15:48:40 +0900

    プッシュ通知実行

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,17 +1,34 @@
 package com.example.demo.controller;
 
+import java.io.IOException;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.example.demo.service.FirebaseClientService;
+import com.google.firebase.messaging.FirebaseMessagingException;
 
 @Controller
 @RequestMapping("/")
 public class IndexController {
 
+	@Autowired
+	private FirebaseClientService firebaseClientService;
+
 	@GetMapping
 	public ModelAndView index(ModelAndView view) {
 		view.setViewName("index");
 		return view;
+	}
+
+	@GetMapping("push")
+	@ResponseBody
+	public String push() throws FirebaseMessagingException, IOException {
+		firebaseClientService.push();
+		return "Push成功";
 	}
 }

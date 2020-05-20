183cc1020f5809c52d1900c0de51f959e866ac3f
c5apple <c5apple_party@yahoo.co.jp>
2020-05-20 13:14:32 +0900

    ブラウザで画像表示

--- src/main/java/com/example/demo/service/FirebaseClientService.java
+++ src/main/java/com/example/demo/service/FirebaseClientService.java
@@ -6,6 +6,8 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.Message;
 import com.google.firebase.messaging.Notification;
+import com.google.firebase.messaging.WebpushConfig;
+import com.google.firebase.messaging.WebpushNotification;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,6 +26,11 @@ public class FirebaseClientService {
 		Message message = Message.builder()
 				.setNotification(new Notification("タイトル", "本文"))
 				.setToken(token)
+				.setWebpushConfig(WebpushConfig.builder()
+						// ブラウザへのメッセージ
+						.setNotification(new WebpushNotification("タイトル", "本文\nです",
+								"https://placehold.jp/150x150.png"))
+						.build())
 				.build();
 
 		// Send a message to the device corresponding to the provided
